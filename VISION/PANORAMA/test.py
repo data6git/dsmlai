3606d4009366417c88c2b7b9fa7fe5bc697d19fe
artur.p <artur.p@lorentus.com>
2019-12-15 11:01:52 +0100

    changes in creation pano from small pieces

--- VISION/PANORAMA/test.py
+++ VISION/PANORAMA/test.py
@@ -30,16 +30,23 @@ class Panorama():
 	def compute_images(self, center):
 		self.params = []
 		self.images_pano = []
-		self.wykorzystane = []
-		for i in range(0,len(self.images)):
-			przes = self.compute_two_images(self.images[0], self.images[i],i, center)
-			if przes is not None:
-				self.params.append(przes)
-				self.images_pano.append(self.images[i])
-				self.wykorzystane.append(i)
-		print(self.wykorzystane)
+		self.wykorzystane = dict()
+		for idx in range(len(self.images)):
+		#for idx in range(5):
+			self.wykorzystane[idx] = []
+			for i in range(0,len(self.images)):
+				przes = self.compute_two_images(self.images[idx], self.images[i])
+				if przes is not None:
+					self.params.append(przes)
+					self.images_pano.append(self.images[i])
+					if i != idx:
+						self.wykorzystane[idx].append(i)
+		print('-'*80)
+		for klucz in self.wykorzystane:
+			print(klucz, self.wykorzystane[klucz])
+		print('-'*80)
 
-	def compute_two_images(self, imageA, imageB, iteracja, center, ratio=0.75, reprojTresh=1.0, showMatches=False):
+	def compute_two_images(self, imageA, imageB, ratio=0.75, reprojTresh=1.0, showMatches=False):
 		(kpsA, featuresA) = self.detectAndDescribe(imageA)
 		(kpsB, featuresB) = self.detectAndDescribe(imageB)
 		if len(kpsA) == 0 or len(kpsB) == 0:
@@ -56,7 +63,7 @@ class Panorama():
 				shift_X.append(kpsA[match[1]][0]-kpsB[match[0]][0])
 				shift_Y.append(kpsA[match[1]][1]-kpsB[match[0]][1])				
 		if len(shift_X)>20 and len(shift_Y)>20:
-			print(len(shift_X), len(shift_Y))
+			#print(len(shift_X), len(shift_Y))
 			przesX = int(sum(shift_X)/len(shift_X))
 			przesY = int(sum(shift_Y)/len(shift_Y))
 			return (przesX, przesY)
@@ -116,14 +123,14 @@ class Panorama():
 				Y_stp = int(Y_str+ratio_Y)
 				#print(image.shape, X_str, X_stp, Y_str, Y_stp)
 				part = image[X_str:X_stp, Y_str:Y_stp]
-				cv2.imshow('image%d'%counter, part)
+				#cv2.imshow('image%d'%counter, part)
 				counter += 1
 				parts.append(part)
 		return parts
 
 	def read_images(self, width=400, str_X=180):
 		self.images = []
-		img_params = [(str_X, 0), (str_X+20, 10)]
+		img_params = [(str_X, 0), (str_X-20, 10), (str_X+20, 10)]
 		for param in img_params:
 			image = self.get_image(self.get_name(param[0], param[1]))
 			image = imutils.resize(image, width=width)
@@ -131,23 +138,71 @@ class Panorama():
 			for part in parts:
 				self.images.append(part)
 
+	def create_tree_based_on_dict(self):
+		#tworzymy drzewo w oparciu o dane ze slownika
+		self.zbior_wszystkich_elementow = set()
+		for element in range(len(self.params)):
+			self.zbior_wszystkich_elementow.add(element)
+		self.pozycje = dict()
+		self.pozycje[0] = [0, 0]
+		for klucz1 in self.wykorzystane:
+			obr1 = self.images[klucz1]
+			for klucz2 in self.wykorzystane[klucz1]:
+				obr2 = self.images[klucz2]
+				wsp = self.compute_two_images(obr1, obr2)
+				print(klucz1, klucz2, wsp)
+
 	def show_pano(self, width=400, str_X=180):
 		self.read_images(width=width, str_X=str_X)
 		self.compute_images(str_X)
-		self.result = np.zeros((3*self.images_pano[0].shape[0],3*self.images_pano[0].shape[1],3), dtype='uint8')
+		#tworzymy zbior wszystkich kawalkow wszystkich obrazkow
+		#iterujemy po self.wykorzystane i zapisujemy wspolrzedne relatywne czyli do klucza
+		#startem jest klocek o id=0
+		#self.create_tree_based_on_dict()
+		self.pozycje = dict()
+		self.pozycje[0] = [0, 0]
+		for klucz1 in self.wykorzystane:
+			obr1 = self.images[klucz1]
+			for klucz2 in self.wykorzystane[klucz1]:
+				if klucz2 not in self.pozycje and klucz1 in self.pozycje:
+					obr2 = self.images[klucz2]
+					wsp = self.compute_two_images(obr1, obr2)
+					if wsp is not None:
+						#print(klucz1, klucz2, wsp[0], wsp[1])
+						wsp_ref = self.pozycje[klucz1]
+						wsp_dst = [0, 0]
+						wsp_dst[0] = wsp_ref[0]+wsp[0]
+						wsp_dst[1] = wsp_ref[1]+wsp[1]
+						self.pozycje[klucz2] = wsp_dst
+		for klucz in sorted(self.pozycje):
+			print(klucz, self.pozycje[klucz])
+		'''
+		for klucz in self.wykorzystane:
+			print(klucz, self.wykorzystane[klucz])
 		for i in range(len(self.params)):
+			print(i, self.params[i],self.images_pano[i].shape[0], self.images_pano[i].shape[1])
+		'''
+		self.result = np.zeros((7*self.images_pano[0].shape[0],7*self.images_pano[0].shape[1],3), dtype='uint8')
+		for i in range(len(self.pozycje)): #bylo params
 			#self.result[self.images[i].shape[0]+self.params[i][1]:2*self.images[i].shape[0]+self.params[i][1], self.images[i].shape[1]+self.params[i][0]:2*self.images[i].shape[1]+self.params[i][0]] = self.images[i]
 			#cv2.imshow('Image%s'%i, self.images_pano[i])
-			print(self.params[i],self.images_pano[i].shape[0], self.images_pano[i].shape[1])
-			str_x = 1*self.images_pano[i].shape[0] + 1*self.params[i][1]
-			stp_x = 2*self.images_pano[i].shape[0] + 1*self.params[i][1]
-			str_y = 1*self.images_pano[i].shape[1] + 1*self.params[i][0]
-			stp_y = 2*self.images_pano[i].shape[1] + 1*self.params[i][0]
-			self.result[str_x:stp_x, str_y:stp_y] = self.images_pano[i]
+			try:
+				#str_x = 3*self.images_pano[i].shape[0] + 1*self.pozycje[i][1]
+				#stp_x = 4*self.images_pano[i].shape[0] + 1*self.pozycje[i][1]
+				#str_y = 3*self.images_pano[i].shape[1] + 1*self.pozycje[i][0]
+				#stp_y = 4*self.images_pano[i].shape[1] + 1*self.pozycje[i][0]
+				str_x = 3*self.images[i].shape[0] + 1*self.pozycje[i][1]
+				stp_x = 4*self.images[i].shape[0] + 1*self.pozycje[i][1]
+				str_y = 3*self.images[i].shape[1] + 1*self.pozycje[i][0]
+				stp_y = 4*self.images[i].shape[1] + 1*self.pozycje[i][0]
+				self.result[str_x:stp_x, str_y:stp_y] = self.images[i]
+				print(i, self.pozycje[i],self.images_pano[i].shape[0], self.images_pano[i].shape[1], str_x, stp_x, str_y, stp_y)
+			except:
+				print(i, 'ERROR')
 		cv2.imshow('Panorama', self.result)
 		cv2.waitKey(0)
-
+		
 
 if __name__ == '__main__':
 	panorama = Panorama('9cgfAbCfJTS9o0wLjnKc-g')
-	panorama.show_pano(640,160)
+	panorama.show_pano(640,200)
